00b70cb4d54c534d6021f0e659db25afc2ae6c39
memoizr <memoizrlabs@gmail.com>
2016-11-06 16:58:38 +0000

    fix visibility of members

--- src/main/scala/fruitmachine/Machine.scala
+++ src/main/scala/fruitmachine/Machine.scala
@@ -1,6 +1,8 @@
 package fruitmachine
 
-case class FruitMachine(slotSpinner: SlotSpinner, costOfSinglePlay: Int, jackPot: Int) {
+case class FruitMachine(private val slotSpinner: SlotSpinner,
+                        private val costOfSinglePlay: Int,
+                        jackPot: Int) {
 
   def play(player: Player): (Option[Player], FruitMachine) = {
     val result: Option[(Option[Player], FruitMachine)] = for {
@@ -14,34 +16,34 @@ case class FruitMachine(slotSpinner: SlotSpinner, costOfSinglePlay: Int, jackPot
       creditedPlayer = paidPlayer.copy(credits = paidPlayer.credits + creditsToCharge)
     } yield (Option(creditedPlayer), copy(jackPot = finalJackpot))
 
-    result getOrElse (None, this)
+    result getOrElse(None, this)
   }
 
-  val kickOutPlayer: Player => Option[Player] = player => player.money match {
+  private val kickOutPlayer: Player => Option[Player] = player => player.money match {
     case money if money >= 0 => Some(player)
     case _ => None
   }
 
-  val moneyThatCanBePaid: Int => Int => Int = moneyInMachine => moneyToPay => {
+  private val moneyThatCanBePaid: Int => Int => Int = moneyInMachine => moneyToPay => {
     if (moneyInMachine >= moneyToPay) moneyToPay else moneyInMachine
   }
 
-  val calculateCreditsToCharge: Int => Int => Int = moneyInMachine => moneyToPay => {
+  private val calculateCreditsToCharge: Int => Int => Int = moneyInMachine => moneyToPay => {
     if (moneyInMachine >= moneyToPay) 0 else moneyToPay - moneyInMachine
   }
 
-  val calculateMoneyLeft: Int => Int => Int = moneyInMachine => moneyToPay => {
+  private val calculateMoneyLeft: Int => Int => Int = moneyInMachine => moneyToPay => {
     if (moneyInMachine >= moneyToPay) moneyInMachine - moneyToPay else 0
   }
 
-  val chargePlayer: Int => Player => Player = costOfPlaying => player =>
+  private val chargePlayer: Int => Player => Player = costOfPlaying => player =>
     if (player.credits > 0) {
       val creditsLeft = player.credits - costOfPlaying max 0
       val moneyToDeduct = costOfPlaying - player.credits max 0
       player.copy(money = player.money - moneyToDeduct, credits = creditsLeft)
     } else player.copy(money = player.money - costOfPlaying)
 
-  val chargeIfApplicable: Int => Player => Option[Player] = kickOutPlayer compose chargePlayer(_)
+  private val chargeIfApplicable: Int => Player => Option[Player] = kickOutPlayer compose chargePlayer(_)
 
   private val payPlayer: Player => Int => Player = (player: Player) => (moneyOwed: Int) => {
     player.copy(money = player.money + moneyOwed)
